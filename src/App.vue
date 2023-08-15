332b2ca2d2b5b0e370296727e2e0117e0c22b22e
Uriel Torres <uriel09890@gmail.com>
2023-08-14 22:29:59 -0600

    Style

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,66 @@
 <template>
-  
+  <div class="container">
+    <br />
+    <div class="row align-items-center">
+      <h1>Welcome to QuoteSymphony</h1>
+    </div>
+    <hr />
+    <div class="row">
+      <div class="row text-center">
+        <div class="col" id="quoteLine">Frase</div>
+      </div>
+      <div class="row text-end">
+        <div class="col" id="authorLine">- Autor</div>
+      </div>
+    </div>
+    <div class="row justify-content-center" id="buttonChangeQuote">
+      <div class="col-auto">
+        <button type="button" class="btn btn-secondary">Change quote</button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+@import url("https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@100&display=swap");
+
+h1,
+button,
+#quoteLine,
+#authorLine {
+  color: white;
+  font-family: "Josefin Sans", sans-serif;
+}
+#quoteLine {
+  font-style: italic;
+  font-size: 1.5rem;
+}
+
+h1 {
+  font-size: 3.5rem;
+}
+
+hr {
+  color: white;
+}
+
+.container {
+  max-width: 800px;
+  width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
+}
+@media (max-width: 768px) {
+  .container {
+    padding: 10px;
+  }
+}
+
+#buttonChangeQuote {
+  padding: 3rem 0rem;
+}
+
+button {
+  background: #414141;
+}
+</style>
